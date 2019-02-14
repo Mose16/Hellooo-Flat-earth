5f47111105fcb43f6dec469e275abff63f17f1e7
Mose16 <47540314+Mose16@users.noreply.github.com>
2019-02-15 12:05:21 +1300

    Add files via upload

---
+++ Bottle.py
@@ -0,0 +1,10 @@
+#LOGS
+#Ver-1.1 --- Added tickets list
+#Ver-1.2 --- Adding server functionality
+
+
+TICKETS = [
+    Ticket("Moses", "moses@gmail.com", "16/11/2001", False),
+    Ticket("Jerry", "moses@gmail.com", "16/11/2001", False),
+    Ticket("Tom", "moses@gmail.com", "16/11/2001", False)
+]
